f597cd87305dbe8a492ebba7786d2518b0bd95fc
marcosantonastasi <marco.santonastasi@gmail.com>
2022-01-18 10:42:35 +0100

    small exercise done

--- 01-class-js_arrays.js
+++ 01-class-js_arrays.js
@@ -5,3 +5,22 @@
 const myArray = ["cappuccino", 48, null, ["marco", "carlos", "carol"] ]
 
 console.log( myArray[3][1] )
+
+console.log( myArray[3].length )
+
+console.log( myArray[1].toString().length )
+
+console.log("return from push: ", myArray.push("2022/01/18") )// myArray[ myArray.length] = "2022/01/18"
+
+console.log("return from pop: ", myArray.pop())
+
+// In your vs-code add your name to the list ["marco", "carlos", "carol"]
+
+myArray[3].push("your name here")
+
+console.log(myArray)
+
+
+
+
+
